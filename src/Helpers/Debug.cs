195c49ec0ab627cd7f5dd454df12cce3b1aacd89
JTnadrooi <jtnadrooi@gmail.com>
2025-05-11 15:42:56 +0200

    added missing stopwatch functionality

--- src/Helpers/Debug.cs
+++ src/Helpers/Debug.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 #nullable enable
 
@@ -7,28 +8,25 @@ namespace AsitLib.Debug
 {
     public interface IDebugStreamStyle
     {
-        public string GetIndentation(int level);
+        public string GetIndentation(int level, bool hasPrefix = false);
         public string GetHeaderIndentation();
     }
 
-    public class BoxDrawingStyle : IDebugStreamStyle
-    {
-        public string GetIndentation(int level)
-        {
-            if (level <= 0) return string.Empty;
-            string prefix = string.Concat(Enumerable.Repeat("│   ", level - 1));
-            return prefix + "├──";
-        }
+    //public class BoxDrawingStyle : IDebugStreamStyle
+    //{
+    //    public string GetIndentation(int level)
+    //    {
+    //        if (level <= 0) return string.Empty;
+    //        string prefix = string.Concat(Enumerable.Repeat("│   ", level - 1));
+    //        return prefix + "├──";
+    //    }
 
-        public string GetHeaderIndentation() => "╭──";
-    }
+    //    public string GetHeaderIndentation() => "╭──";
+    //}
 
     public class DefaultStyle : IDebugStreamStyle
     {
-        public string GetIndentation(int level)
-        {
-            return new string(' ', level * 4) + "^---";
-        }
+        public string GetIndentation(int level, bool hasPrefix = false) => new string(' ', level * 4) + "^" + (hasPrefix ? string.Empty : "---");
         public string GetHeaderIndentation() => "^";
     }
 
@@ -36,13 +34,17 @@ namespace AsitLib.Debug
     {
         private IDebugStreamStyle style;
         private int indentation = 0;
+        private Stopwatch?[] stopwatches;
+        private int maxDepth;
 
         public bool Silent { get; set; } = false;
 
-        public DebugStream(string? styleId = null, string? header = null)
+        public DebugStream(string? styleId = null, string? header = null, int maxDepth = 20)
         {
-            this.style = GetStyle(styleId);
+            style = GetStyle(styleId);
             if (!string.IsNullOrEmpty(header)) Header(header);
+            this.maxDepth = maxDepth;
+            stopwatches = new Stopwatch[maxDepth];
         }
 
         public void Header(string msg)
@@ -50,35 +52,37 @@ namespace AsitLib.Debug
             Console.WriteLine(style.GetHeaderIndentation() + "[" + (msg ?? string.Empty).ToUpperInvariant() + "]");
         }
 
-        public void Log(string msg, object[]? displays = null)
+        public void Log(string? msg, object?[]? displays = null)
         {
             if (Silent) return;
 
-            msg = msg ?? string.Empty;
             int delta = 0;
 
-            // adjust delta for closing marker '<'.
+            msg = msg ?? "_NULL_";
+            if (msg.StartsWith("\t")) throw new Exception();
             if (msg.StartsWith("<"))
             {
                 delta--;
                 msg = msg.Substring(1).TrimStart();
             }
-            // adjust delta for opening marker '>'.
             else if (msg.StartsWith(">"))
             {
                 delta++;
                 msg = msg.Substring(1).TrimStart();
             }
-
-            // count all leading tabs as additional indent.
-            int tabs = msg.TakeWhile(c => c == '\t').Count();
-            if (tabs > 0)
+            // prefix handling.
+            char? prefix = (msg[0] == '[' && msg[2] == ']') ? msg[1] : null;
+            switch (prefix)
             {
-                delta += tabs;
-                msg = msg.Substring(tabs);
+                case 's':
+                    stopwatches[indentation + 1] = Stopwatch.StartNew();
+                    Console.WriteLine(prefix);
+                    break;
+                default: 
+                    break;
             }
 
-            // append display info if provided.
+            if (delta > 0) msg = msg.TrimEnd('.') + "..";
             if (displays != null)
             {
                 string list;
@@ -86,7 +90,7 @@ namespace AsitLib.Debug
                 {
                     IEnumerable<string> displayTypes = displays.Select(d =>
                     {
-                        if (d == null) return "undefined";
+                        if (d == null) return "_NULL_";
                         return d is string str ? ("\"" + str + "\"") : d.ToString()!; 
                     });
                     list = string.Join(", ", displayTypes);
@@ -94,24 +98,29 @@ namespace AsitLib.Debug
                 else list = "_EMPTY_ARRAY_";
                 msg += " [" + list + "]";
             }
-
-            string indentStr = style.GetIndentation(indentation);
+            string indentStr = style.GetIndentation(indentation, prefix != null);
             Console.WriteLine(indentStr + msg);
             indentation = Math.Max(indentation + delta, 0);
         }
 
-        public void Error(string msg, object[]? displays = null)
+        public void Warn(string msg, object[]? displays = null)
         {
             ConsoleColor origColor = Console.ForegroundColor;
             Console.ForegroundColor = ConsoleColor.Red;
-            Log("WARNING: " + (msg ?? string.Empty), displays);
+            Log("warning: " + (msg ?? string.Empty), displays);
             Console.ForegroundColor = origColor;
+        }
+
+        public void Succes()
+        {
+            Log("<succes: time taken: " + (stopwatches[indentation]?.ElapsedMilliseconds.ToString() ?? "??") + "ms.");
+            stopwatches[indentation] = null;
         }
 
         public static IDebugStreamStyle GetStyle(string? styleId = null) =>
             (styleId ?? "Default") switch
             {
-                "BoxDrawing" => new BoxDrawingStyle(),
+                // "BoxDrawing" => new BoxDrawingStyle(),
                 "Default" => new DefaultStyle(),
                 _ => throw new ArgumentException()
             };
