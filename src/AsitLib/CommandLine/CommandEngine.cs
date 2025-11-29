ad8118a96b09b51085b6ac4b6c21f743d4e23d61
JTnadrooi <jtnadrooi@gmail.com>
2025-11-29 20:56:50 +0100

    Move CommandInfoFactory to seperate file

--- src/AsitLib/CommandLine/CommandEngine.cs
+++ src/AsitLib/CommandLine/CommandEngine.cs
@@ -15,23 +15,6 @@ using static System.Net.WebRequestMethods;
 
 namespace AsitLib.CommandLine
 {
-    public interface ICommandInfoFactory<in TAttribute, out TCommandInfo> where TAttribute : CommandAttribute where TCommandInfo : CommandInfo
-    {
-        public TCommandInfo Convert(TAttribute attribute, CommandProvider provider, MethodInfo methodInfo);
-    }
-
-    public static class CommandInfoFactory
-    {
-        public class DefaultInfoFactory : ICommandInfoFactory<CommandAttribute, ProviderCommandInfo>
-        {
-            public DefaultInfoFactory() { }
-            public ProviderCommandInfo Convert(CommandAttribute attribute, CommandProvider provider, MethodInfo methodInfo)
-                => new ProviderCommandInfo(CommandHelpers.CreateCommandId(attribute, provider, methodInfo).ToSingleArray().Concat(attribute.Aliases).ToArray(), attribute, methodInfo, provider);
-        }
-
-        public static ICommandInfoFactory<CommandAttribute, CommandInfo> Default { get; } = new DefaultInfoFactory();
-    }
-
     public class CommandEngine : IDisposable
     {
         private bool _disposedValue;
---
+++ src/AsitLib/CommandLine/CommandInfoFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsitLib.CommandLine
+{
+    public interface ICommandInfoFactory<in TAttribute, out TCommandInfo> where TAttribute : CommandAttribute where TCommandInfo : CommandInfo
+    {
+        public TCommandInfo Convert(TAttribute attribute, CommandProvider provider, MethodInfo methodInfo);
+    }
+
+    public static class CommandInfoFactory
+    {
+        public class DefaultInfoFactory : ICommandInfoFactory<CommandAttribute, ProviderCommandInfo>
+        {
+            public DefaultInfoFactory() { }
+            public ProviderCommandInfo Convert(CommandAttribute attribute, CommandProvider provider, MethodInfo methodInfo)
+                => new ProviderCommandInfo(CommandHelpers.CreateCommandId(attribute, provider, methodInfo).ToSingleArray().Concat(attribute.Aliases).ToArray(), attribute, methodInfo, provider);
+        }
+
+        public static ICommandInfoFactory<CommandAttribute, CommandInfo> Default { get; } = new DefaultInfoFactory();
+    }
+}
