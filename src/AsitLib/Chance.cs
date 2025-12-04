9b8c1f16e07c61f7f32bf6cc5db02c52204603bf
JTnadrooi <jtnadrooi@gmail.com>
2025-12-04 18:13:34 +0100

    Add AsitLib.Chance namespace

---
+++ src/AsitLib/Chance.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using static AsitLib.StringHelpers;
+
+namespace AsitLib.Chance
+{
+    public static class RandomExtensions
+    {
+        public static bool NextBool(this Random rand, double chance = 0.5)
+        {
+            return rand.NextDouble() < chance;
+        }
+
+        public static T PickFrom<T>(this Random rand, IEnumerable<T> values)
+            => values is IReadOnlyList<T> list ? list[rand.Next(list.Count)] : values.OrderBy(x => rand.Next()).First();
+
+        public static T PickFrom<T>(this Random random, IReadOnlyDictionary<T, int> values)
+        {
+            int randomValue = random.Next(values.Values.Sum());
+            int cumulativeWeight = 0;
+
+            foreach (var kvp in values)
+            {
+                cumulativeWeight += kvp.Value;
+                if (randomValue < cumulativeWeight)
+                {
+                    return kvp.Key;
+                }
+            }
+
+            throw new InvalidOperationException("Unexpected error in weighted random selection.");
+        }
+    }
+}
