7522b00ba0d08177375fc4a158a7536b92a27982
JTnadrooi <jtnadrooi@gmail.com>
2025-12-05 17:08:50 +0100

    Move ITuple extension to seperate file

---
+++ src/AsitLib/Extensions/TupleExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsitLib
+{
+    public static class TupleExtensions
+    {
+        public static object?[] GetItems(this ITuple tuple)
+        {
+            object?[] toret = new object?[tuple.Length];
+            for (int i = 0; i < tuple.Length; i++) toret[i] = tuple[i];
+            return toret;
+        }
+    }
+}
