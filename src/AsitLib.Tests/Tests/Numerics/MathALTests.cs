44c07ae9e636433dcf41bce956e92cb0b7a561f0
JTnadrooi <jtnadrooi@gmail.com>
2025-12-05 21:19:32 +0100

    Add AsitLib.Numerics tests

---
+++ src/AsitLib.Tests/Tests/Numerics/MathALTests.cs
@@ -0,0 +1,168 @@
+﻿using AsitLib.Numerics;
+using System;
+using System.Numerics;
+
+namespace AsitLib.Tests
+{
+    [TestClass]
+    public class MathALTests
+    {
+        [TestMethod]
+        public void Invert_ValidValue()
+        {
+            float result = MathAL.Invert(5f, 0f);
+            Assert.AreEqual(-5f, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Invert_InvalidValue_NaN()
+        {
+            MathAL.Invert(float.NaN, 0f);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Invert_InvalidValue_PositiveInfinity()
+        {
+            MathAL.Invert(float.PositiveInfinity, 0f);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Invert_InvalidValue_NegativeInfinity()
+        {
+            MathAL.Invert(float.NegativeInfinity, 0f);
+        }
+
+        [TestMethod]
+        public void Normalize_Degrees_ValidValue()
+        {
+            float result = MathAL.Normalize(370f);
+            Assert.AreEqual(10f, result);
+        }
+
+        [TestMethod]
+        public void Normalize_Radians_ValidValue()
+        {
+            float result = MathAL.Normalize((float)Math.PI * 2 + 0.5f, true);
+            Assert.AreEqual(0.5f, result, 0.0001f);
+        }
+
+        [TestMethod]
+        public void Difference_ValidValues()
+        {
+            float result = MathAL.Difference(10f, 5f);
+            Assert.AreEqual(5f, result);
+        }
+
+        [TestMethod]
+        public void Average_ValidValues()
+        {
+            float result = MathAL.Average(10f, 20f, 30f);
+            Assert.AreEqual(20f, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Average_InvalidValue_NaN()
+        {
+            MathAL.Average(10f, 20f, float.NaN);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Average_InvalidValue_Infinity()
+        {
+            MathAL.Average(10f, float.PositiveInfinity, 20f);
+        }
+
+        [TestMethod]
+        public void IsNumberic_TypeValid()
+        {
+            bool result = MathHelperAL.IsNumberic(typeof(int));
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsNumberic_TypeInvalid()
+        {
+            bool result = MathHelperAL.IsNumberic(typeof(string));
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IsNumberic_ObjectValid()
+        {
+            bool result = MathHelperAL.IsNumberic(10);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsNumberic_ObjectInvalid()
+        {
+            bool result = MathHelperAL.IsNumberic("test");
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ToBigInteger_Int128()
+        {
+            Int128 value = 12345678901234567890;
+            BigInteger result = MathHelperAL.ToBigInteger(value);
+            Assert.AreEqual((BigInteger)value, result);
+        }
+
+        [TestMethod]
+        public void ToBigInteger_UInt128()
+        {
+            UInt128 value = 12345678901234567890U;
+            BigInteger result = MathHelperAL.ToBigInteger(value);
+            Assert.AreEqual((BigInteger)value, result);
+        }
+
+        [TestMethod]
+        public void ToBigInteger_Int()
+        {
+            int value = 123;
+            BigInteger result = MathHelperAL.ToBigInteger(value);
+            Assert.AreEqual(new BigInteger(123), result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ToBigInteger_InvalidType()
+        {
+            string value = "test";
+            MathHelperAL.ToBigInteger(value);
+        }
+
+        [TestMethod]
+        public void IsNear_True()
+        {
+            bool result = MathHelperAL.IsNear(5f, 5.05f, 0.1f);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsNear_False()
+        {
+            bool result = MathHelperAL.IsNear(5f, 6f, 0.1f);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void AddPostfix_5000()
+        {
+            string result = MathHelperAL.AddPostfix(5000);
+            Assert.AreEqual("5k", result);
+        }
+
+        [TestMethod]
+        public void AddPostfix_500000000()
+        {
+            string result = MathHelperAL.AddPostfix(500_000_000);
+            Assert.AreEqual("500M", result);
+        }
+    }
+}
---
+++ src/AsitLib.Tests/Tests/Numerics/NormalizedRangeTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AsitLib.Numerics.Tests
+{
+    [TestClass]
+    public class NormalizedRangeTests
+    {
+        [TestMethod]
+        public void NormalizedRange_Constructor_ValidRange()
+        {
+            NormalizedRange range = new NormalizedRange(1, 5);
+            Assert.AreEqual(1, range.Start);
+            Assert.AreEqual(5, range.End);
+            Assert.AreEqual(4, range.Lenght);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void NormalizedRange_Constructor_InvalidRange()
+        {
+            NormalizedRange range = new NormalizedRange(5, 1);
+        }
+
+        [TestMethod]
+        public void NormalizedRange_Equals_True()
+        {
+            NormalizedRange range1 = new NormalizedRange(1, 5);
+            NormalizedRange range2 = new NormalizedRange(1, 5);
+            Assert.IsTrue(range1.Equals(range2));
+        }
+
+        [TestMethod]
+        public void NormalizedRange_Equals_False()
+        {
+            NormalizedRange range1 = new NormalizedRange(1, 5);
+            NormalizedRange range2 = new NormalizedRange(2, 6);
+            Assert.IsFalse(range1.Equals(range2));
+        }
+
+        [TestMethod]
+        public void NormalizedRange_IsEmpty_True()
+        {
+            NormalizedRange range = new NormalizedRange(1, 1);
+            Assert.IsTrue(range.IsEmpty);
+        }
+
+        [TestMethod]
+        public void NormalizedRange_IsEmpty_False()
+        {
+            NormalizedRange range = new NormalizedRange(1, 5);
+            Assert.IsFalse(range.IsEmpty);
+        }
+
+        [TestMethod]
+        public void NormalizedRange_Contains_True()
+        {
+            NormalizedRange range = new NormalizedRange(1, 5);
+            Assert.IsTrue(range.Contains(3));
+        }
+
+        [TestMethod]
+        public void NormalizedRange_Contains_False()
+        {
+            NormalizedRange range = new NormalizedRange(1, 5);
+            Assert.IsFalse(range.Contains(6));
+        }
+
+        [TestMethod]
+        public void NormalizedRange_AsRange()
+        {
+            NormalizedRange range = new NormalizedRange(1, 5);
+            Assert.AreEqual("1..5", range.ToString());
+        }
+
+        [TestMethod]
+        public void NormalizedRange_ImplicitOperator()
+        {
+            NormalizedRange range = new NormalizedRange(1, 5);
+            Range convertedRange = range;
+            Assert.AreEqual("1..5", convertedRange.ToString());
+        }
+
+        [TestMethod]
+        public void NormalizedRange_GetFromValues()
+        {
+            NormalizedRange range = NormalizedRange.GetFromValues(5, 1);
+            Assert.AreEqual(1, range.Start);
+            Assert.AreEqual(5, range.End);
+        }
+
+        [TestMethod]
+        public void NormalizedRange_CreateFromFactory()
+        {
+            Range range = new Range(1, 5);
+            int[] values = { 1, 2, 3, 4, 5 };
+            NormalizedRange normalizedRange = NormalizedRange.CreateFromFactory(range, values);
+            Assert.AreEqual(1, normalizedRange.Start);
+            Assert.AreEqual(5, normalizedRange.End);
+        }
+    }
+}
