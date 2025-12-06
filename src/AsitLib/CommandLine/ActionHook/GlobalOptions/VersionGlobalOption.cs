f3bc3be322a1ec9a2c5b1d2a80ad60bc030a304f
JTnadrooi <jtnadrooi@gmail.com>
2025-12-06 15:33:04 +0100

    Add VersionGlobalOption

---
+++ src/AsitLib/CommandLine/ActionHook/GlobalOptions/VersionGlobalOption.cs
@@ -0,0 +1,28 @@
+﻿using AsitLib.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsitLib.CommandLine
+{
+    public sealed class VersionGlobalOption : GlobalOption
+    {
+        private string _versionString;
+
+        public VersionGlobalOption(Version version) : this(version.ToString()) { }
+        public VersionGlobalOption(string versionString) : base("version", "Print version.")
+        {
+            _versionString = versionString;
+        }
+
+        public override void PreCommand(CommandContext context)
+        {
+            context
+                .AddFlag(ExecutingContextFlags.FullStop)
+                .AddAction(() => _versionString);
+        }
+    }
+}
