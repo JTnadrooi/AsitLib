f6ff1b8be4e84844bb72c7563a66f46a67a2b064
JTnadrooi <jtnadrooi@gmail.com>
2025-12-08 20:26:16 +0100

    Add more tryget extension methods

--- src/AsitLib/Extensions/EnumerableExtensions.cs
+++ src/AsitLib/Extensions/EnumerableExtensions.cs
@@ -11,6 +11,91 @@ namespace AsitLib
 {
     public static class EnumerableExtensions
     {
+        /// <summary>
+        /// Attempts to get the first element.
+        /// </summary>
+        public static bool TryGetFirst<T>(this IEnumerable<T> source, [NotNullWhen(true)] out T? value)
+            => TryGetFirst(source, v => true, out value);
+
+        /// <summary>
+        /// Attempts to get the first element matching a predicate.
+        /// </summary>
+        public static bool TryGetFirst<T>(this IEnumerable<T> source, Func<T, bool> predicate, [NotNullWhen(true)] out T? value)
+        {
+            foreach (T element in source)
+            {
+                if (!predicate(element)) continue;
+
+                value = element!;
+                return true;
+            }
+
+            value = default;
+            return false;
+        }
+
+        /// <summary>
+        /// Attempts to get the last element.
+        /// </summary>
+        public static bool TryGetLast<T>(this IEnumerable<T> source, [NotNullWhen(true)] out T? value)
+            => TryGetLast<T>(source, v => true, out value);
+
+        /// <summary>
+        /// Attempts to get the last element matching a predicate.
+        /// </summary>
+        public static bool TryGetLast<T>(this IEnumerable<T> source, Func<T, bool> predicate, [NotNullWhen(true)] out T? value)
+        {
+            bool found = false;
+            T? last = default;
+
+            foreach (T element in source)
+            {
+                if (!predicate(element)) continue;
+
+                last = element;
+                found = true;
+            }
+
+            value = found ? last : default;
+            return found;
+        }
+
+        /// <summary>
+        /// Attempts to get the element at the specified index.
+        /// </summary>
+        public static bool TryGetAt<T>(this IEnumerable<T> source, int index, [NotNullWhen(true)] out T? value)
+            => TryGetAt(source, index, v => true, out value);
+
+        /// <summary>
+        /// Attempts to get the first element at the specified index that matches a predicate.
+        /// </summary>
+        public static bool TryGetAt<T>(this IEnumerable<T> source, int index, Func<T, bool> predicate, [NotNullWhen(true)] out T? value)
+        {
+            if (index < 0)
+            {
+                value = default;
+                return false;
+            }
+
+            int matchIndex = 0;
+
+            foreach (T element in source)
+            {
+                if (!predicate(element)) continue;
+
+                if (matchIndex == index)
+                {
+                    value = element!;
+                    return true;
+                }
+
+                matchIndex++;
+            }
+
+            value = default;
+            return false;
+        }
+
         public static bool EndsWith<T>(this T[] array1, T[] array2)
         {
             if (array2.Length > array1.Length) throw new ArgumentException($"Lenght exeeds base array lenght.", nameof(array2));
