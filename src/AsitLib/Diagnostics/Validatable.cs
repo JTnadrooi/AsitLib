de2f6ac0fc8fa0e49b72065ed566e38b312e1c18
JTnadrooi <jtnadrooi@gmail.com>
2025-12-21 12:26:32 +0100

    General Validatable improvements

--- src/AsitLib/Diagnostics/Validatable.cs
+++ src/AsitLib/Diagnostics/Validatable.cs
@@ -19,33 +19,4 @@
             }
         }
     }
-
-    public class InvalidObjectException : Exception
-    {
-        public IReadOnlyList<InvalidReason> Reasons { get; }
-
-        public InvalidObjectException(IReadOnlyList<InvalidReason> reasons) : base($"Invalid state, reasons: '{reasons.ToJoinedString(", ")}'.")
-        {
-            if (reasons.Count == 0) throw new ArgumentException("'reasons' cannot be empty.", nameof(reasons));
-
-            Reasons = reasons;
-        }
-    }
-
-    public struct InvalidReason
-    {
-        public string Message { get; }
-
-        public InvalidReason(string msg) => Message = msg;
-
-        public static bool operator ==(InvalidReason left, InvalidReason right) => left.Message == right.Message;
-        public static bool operator !=(InvalidReason left, InvalidReason right) => left.Message != right.Message;
-
-        public static implicit operator InvalidReason(string msg) => new InvalidReason(msg);
-        public static implicit operator string(InvalidReason invalidReason) => invalidReason.Message;
-
-        public override bool Equals(object? obj) => obj is InvalidReason other && this == other;
-        public override int GetHashCode() => Message?.GetHashCode() ?? 0;
-        public override string ToString() => $"{{Message: '{Message}'}}";
-    }
 }
---
+++ src/AsitLib/Diagnostics/Validation/InvalidObjectException.cs
@@ -0,0 +1,38 @@
+﻿namespace AsitLib.Diagnostics
+{
+    public class InvalidObjectException : Exception
+    {
+        public IReadOnlyList<InvalidReason> Reasons { get; }
+
+        public InvalidObjectException(string message) : this(new InvalidReason(message)) { }
+
+        public InvalidObjectException(params IReadOnlyList<InvalidReason> reasons) : base($"Invalid state, reasons: '{reasons.ToJoinedString(", ")}'.")
+        {
+            if (reasons.Count == 0) throw new ArgumentException("'reasons' cannot be empty.", nameof(reasons));
+
+            Reasons = reasons;
+        }
+    }
+
+    public struct InvalidReason
+    {
+        public string Message { get; }
+
+        public InvalidReason(string message)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(message);
+
+            Message = message;
+        }
+
+        public static bool operator ==(InvalidReason left, InvalidReason right) => left.Message == right.Message;
+        public static bool operator !=(InvalidReason left, InvalidReason right) => left.Message != right.Message;
+
+        public static implicit operator InvalidReason(string msg) => new InvalidReason(msg);
+        public static implicit operator string(InvalidReason invalidReason) => invalidReason.Message;
+
+        public override bool Equals(object? obj) => obj is InvalidReason other && this == other;
+        public override int GetHashCode() => Message?.GetHashCode() ?? 0;
+        public override string ToString() => $"{{Message: '{Message}'}}";
+    }
+}
