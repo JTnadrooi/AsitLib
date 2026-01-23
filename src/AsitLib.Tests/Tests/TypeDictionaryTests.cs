6c8c406f6a27052835b74411f13dc7ba764d15fe
JTnadrooi <jtnadrooi@gmail.com>
2026-01-23 17:02:26 +0100

    Add TypeDictionary

---
+++ src/AsitLib.Tests/Tests/TypeDictionaryTests.cs
@@ -0,0 +1,119 @@
+﻿namespace AsitLib.Tests
+{
+    [TestClass]
+    public class TypeDictionaryTests
+    {
+        public interface IAnimal { }
+        public class Dog : IAnimal { }
+        public class Cat : IAnimal { }
+        public class GoldenRetriever : Dog { }
+
+        [TestMethod]
+        public void Add_ImplementingType()
+        {
+            TypeDictionary<IAnimal> dictionary = new TypeDictionary<IAnimal>();
+            Dog dog = new Dog();
+
+            dictionary.Add<Dog>(dog);
+
+            dictionary.Should().HaveCount(1);
+            dictionary.ContainsKey(typeof(Dog)).Should().BeTrue();
+            dictionary[typeof(Dog)].Should().BeSameAs(dog);
+        }
+
+        [TestMethod]
+        public void GetValue_TypeInDictionary_GetsValueWithTypeAsKey()
+        {
+            TypeDictionary<IAnimal> dictionary = new TypeDictionary<IAnimal>();
+            Dog dog = new Dog();
+
+            dictionary.Add<Dog>(dog);
+
+            dictionary.GetValue<Dog>().Should().BeSameAs(dog);
+        }
+
+        [TestMethod]
+        public void ContainsType_ParentClassThatsNotAdded_ReturnsFalse()
+        {
+            TypeDictionary<IAnimal> dictionary = new TypeDictionary<IAnimal>();
+            dictionary.Add<Dog>(new Dog());
+
+            dictionary.ContainsType<IAnimal>().Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void ContainsType_AddedType_ReturnsTrue()
+        {
+            TypeDictionary<IAnimal> dictionary = new TypeDictionary<IAnimal>();
+            dictionary.Add<Dog>(new Dog());
+
+            dictionary.ContainsType<Dog>().Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Add_DerivedType_OnlyAddsForExactType()
+        {
+            TypeDictionary<IAnimal> dictionary = new TypeDictionary<IAnimal>();
+            GoldenRetriever golden = new GoldenRetriever();
+
+            dictionary.Add<GoldenRetriever>(golden);
+
+            dictionary.ContainsKey(typeof(GoldenRetriever)).Should().BeTrue();
+            dictionary.ContainsKey(typeof(IAnimal)).Should().BeFalse();
+            dictionary.ContainsKey(typeof(Dog)).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Add_MultipleDifferentTypes()
+        {
+            TypeDictionary<IAnimal> dictionary = new TypeDictionary<IAnimal>();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            dictionary.Add<Dog>(dog);
+            dictionary.Add<Cat>(cat);
+
+            dictionary.Should().HaveCount(2);
+            dictionary.ContainsKey(typeof(Dog)).Should().BeTrue();
+            dictionary.ContainsKey(typeof(Cat)).Should().BeTrue();
+            dictionary[typeof(Dog)].Should().BeSameAs(dog);
+            dictionary[typeof(Cat)].Should().BeSameAs(cat);
+        }
+
+        [TestMethod]
+        public void Add_Duplicate_ThrowsEx()
+        {
+            TypeDictionary<IAnimal> dictionary = new TypeDictionary<IAnimal>();
+            Dog dog1 = new Dog();
+            Dog dog2 = new Dog();
+
+            dictionary.Add<Dog>(dog1);
+
+            Invoking(() => dictionary.Add<Dog>(dog2)).Should().Throw<ArgumentException>();
+        }
+
+        //[TestMethod]
+        //public void Add_TypeNotAssignableToParentType_ThrowsEx()
+        //{
+        //    TypeDictionary<Dog> dictionary = new TypeDictionary<Dog>();
+        //    Dog dog = new Dog();
+        //    GoldenRetriever golden = new GoldenRetriever();
+        //    Cat cat = new Cat();
+
+        //    dictionary.Add(dog);
+        //    dictionary.Add(golden);
+
+        //    Invoking(() => dictionary.Add(cat.GetType(), cat)).Should().Throw<ArgumentException>();
+        //}
+
+        [TestMethod]
+        public void Add_Null_ThrowsEx()
+        {
+            TypeDictionary<IAnimal> dictionary = new TypeDictionary<IAnimal>();
+
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+            Invoking(() => dictionary.Add<Dog>(null)).Should().Throw<ArgumentNullException>();
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+        }
+    }
+}
---
+++ src/AsitLib/TypeDictionary.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsitLib
+{
+    public sealed class TypeDictionary<TParentType> : IDictionary<Type, TParentType>,
+        ICollection<KeyValuePair<Type, TParentType>>,
+        IEnumerable<KeyValuePair<Type, TParentType>>,
+        IReadOnlyCollection<KeyValuePair<Type, TParentType>>,
+        IReadOnlyDictionary<Type, TParentType>
+    {
+        private readonly Dictionary<Type, TParentType> _dictionary;
+
+        public TParentType this[Type key]
+        {
+            get => _dictionary[key];
+            set => _dictionary[key] = value;
+        }
+
+        public TypeDictionary(int capacity)
+        {
+            _dictionary = new Dictionary<Type, TParentType>(capacity);
+        }
+
+        public TypeDictionary()
+        {
+            _dictionary = new Dictionary<Type, TParentType>();
+        }
+
+        public ICollection<Type> Keys => _dictionary.Keys;
+        public ICollection<TParentType> Values => _dictionary.Values;
+        public int Count => _dictionary.Count;
+        public bool IsReadOnly => false;
+
+        IEnumerable<Type> IReadOnlyDictionary<Type, TParentType>.Keys => ((IReadOnlyDictionary<Type, TParentType>)_dictionary).Keys;
+
+        IEnumerable<TParentType> IReadOnlyDictionary<Type, TParentType>.Values => ((IReadOnlyDictionary<Type, TParentType>)_dictionary).Values;
+
+        private void ThrowIfInvalidKey(Type key, TParentType value)
+        {
+            ArgumentNullException.ThrowIfNull(key);
+            ArgumentNullException.ThrowIfNull(value);
+
+            if (!key.IsAssignableTo(typeof(TParentType))) throw new InvalidOperationException($"{key} is not assignable to {typeof(TParentType)}");
+        }
+
+        public void Add<T>(T value) where T : TParentType => Add(typeof(T), value);
+        public void Add(KeyValuePair<Type, TParentType> item) => Add(item.Key, item.Value);
+        public void Add(Type key, TParentType value)
+        {
+            ThrowIfInvalidKey(key, value);
+
+            _dictionary.Add(key, value);
+        }
+
+        public T GetValue<T>() where T : TParentType => (T)this[typeof(T)]!;
+
+        public void Clear() => _dictionary.Clear();
+
+        public bool Contains(KeyValuePair<Type, TParentType> item) => _dictionary.Contains(item);
+        public bool ContainsType<T>() where T : TParentType => ContainsKey(typeof(T));
+        public bool ContainsKey(Type key) => _dictionary.ContainsKey(key);
+
+        public void CopyTo(KeyValuePair<Type, TParentType>[] array, int arrayIndex) =>
+            ((ICollection<KeyValuePair<Type, TParentType>>)_dictionary).CopyTo(array, arrayIndex);
+
+        public IEnumerator<KeyValuePair<Type, TParentType>> GetEnumerator() => _dictionary.GetEnumerator();
+
+        public void Remove<T>() where T : TParentType => Remove(typeof(T));
+        public bool Remove(Type key) => _dictionary.Remove(key);
+        public bool Remove(KeyValuePair<Type, TParentType> item) => _dictionary.Remove(item.Key);
+
+        public bool TryGetValue<T>([MaybeNullWhen(false)] out TParentType value) => TryGetValue(typeof(T), out value);
+
+        public bool TryGetValue(Type key, [MaybeNullWhen(false)] out TParentType value) => _dictionary.TryGetValue(key, out value);
+
+        IEnumerator IEnumerable.GetEnumerator() => _dictionary.GetEnumerator();
+    }
+}
